10d5f671580e1227e679732adf6f4ac962746c01
kutay yaman <yamankutay1@gmail.com>
2021-06-29 14:00:24 +0300

    project initialized

---
+++ src/main/java/com/kutayyaman/footballListCase/FootballListCaseApplication.java
@@ -0,0 +1,13 @@
+package com.kutayyaman.footballListCase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FootballListCaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FootballListCaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kutayyaman/footballListCase/FootballListCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kutayyaman.footballListCase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FootballListCaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
