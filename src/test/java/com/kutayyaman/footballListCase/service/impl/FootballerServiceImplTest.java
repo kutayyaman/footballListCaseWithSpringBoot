e93c0b621ce2375549e107849eaecf9245042f15
Kutay Yaman <yamankutay1@gmail.com>
2021-10-13 12:29:16 +0300

    I Added a dummy test to test my bamboo can run the test

---
+++ src/test/java/com/kutayyaman/footballListCase/service/impl/FootballerServiceImplTest.java
@@ -0,0 +1,18 @@
+package com.kutayyaman.footballListCase.service.impl;
+
+import com.kutayyaman.footballListCase.domain.Footballer;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FootballerServiceImplTest {
+
+    @Test
+    void findAllFootballerByTeamId(){
+        Footballer footballer = new Footballer();
+        footballer.setFootballerName("Kutay");
+
+        assertEquals("Kutay",footballer.getFootballerName());
+    }
+
+}
